9f74fa94ad5ecd3717e86a5d42fed26a13f83969
nvelezunilasallista <nvelez@unilasallista.edu.co>
2026-08-31 11:23:33 -0500

    validaciones

--- proyecto1/app/src/main/java/com/example/proyecto1/LoginActivity.kt
+++ proyecto1/app/src/main/java/com/example/proyecto1/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.example.proyecto1
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,11 +21,21 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
 
-        val btnLogin = findViewById<Button>(R.id.button);
+        val btnLogin = findViewById<Button>(R.id.button)
+        val etEmail = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val etPassword = findViewById<EditText>(R.id.editTextTextPassword)
 
         btnLogin.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                val intent = Intent(this, HomeActivity::class.java)
+                intent.putExtra("USER_EMAIL", email)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
